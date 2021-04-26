9874440ddfdb5947136b8c77596a7c499e2d7cdc
wq <kyu21@outlook.com>
2021-04-26 23:50:29 +0900

    Probe!

--- spring/src/main/java/com/example/spring/api/HealthApi.java
+++ spring/src/main/java/com/example/spring/api/HealthApi.java
@@ -1,14 +1,21 @@
 package com.example.spring.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthApi {
 
     @GetMapping("/health")
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/health2")
+    public ResponseEntity<String> health2() {
+        return ResponseEntity.badRequest().build();
     }
 }
